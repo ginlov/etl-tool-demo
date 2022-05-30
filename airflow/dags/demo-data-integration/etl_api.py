e3abcd443a05f56bff2c24d7404d40ac51b8f574
Giang Vu Long <longgiang2015bn@gmail.com>
2022-05-30 10:10:41 +0700

    feat: add loading to mysql api

--- airflow/dags/demo-data-integration/etl_api.py
+++ airflow/dags/demo-data-integration/etl_api.py
@@ -10,11 +10,12 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.models import Variable
 from airflow.utils.dates import days_ago
-from operators.api_etl import crawl_job, transform_data
+from operators.api_etl import crawl_job, transform_data, to_mysql
 from airflow.operators.python_operator import PythonOperator, PythonVirtualenvOperator
 from airflow.providers.mysql.operators.mysql import MySqlOperator
 
 requirements=[
+    "pymysql",
     "pandas"
 ]
 
@@ -31,15 +32,20 @@ with DAG(
     default_args=args,
     schedule_interval="@daily"
 ) as dag:
-    # crawl_task = PythonVirtualenvOperator(
-        # task_id="crawl",
-        # requirements=requirements,
-        # python_callable=crawl_job
-    # )
+    crawl_task = PythonVirtualenvOperator(
+        task_id="crawl",
+        requirements=requirements,
+        python_callable=crawl_job
+    )
 
     transform_task = PythonVirtualenvOperator(
         task_id="transform",
         requirements=requirements,
         python_callable=transform_data
     )
-    # crawl_task >> transform_task
+    load_to_mysql = PythonVirtualenvOperator(
+        task_id="load_to_mysql",
+        requirements=requirements,
+        python_callable=to_mysql
+    )
+    crawl_task >> transform_task >> load_to_mysql
